b19636ddf1b0b18c51a012c61fe874d450dbae97
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-03-27 19:55:54 +0100

    Initial commit

---
+++ php/page02.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 02</title>
+
+</head>
+
+
+<body>
+
+     
+    <h1>Sessions</h1>
+    <h2>Debug</h2>
+        <pre>
+            <?php var_dump($_SESSION); ?>
+        </pre>
+
+
+</body>
+</html>
+
+
